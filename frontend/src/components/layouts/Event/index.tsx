3a9e30026def557dcb8e54134ca86caa3f28d529
gattaloukik123 <loukik.gatta@gmail.com>
2025-03-26 21:42:37 +0530

    changed logo to Communitie logo

--- frontend/src/components/layouts/Event/index.tsx
+++ frontend/src/components/layouts/Event/index.tsx
@@ -117,7 +117,7 @@ const EventLayout = () => {
                 </div>
                 <div className={classes.logo}>
                     <NavLink to={'/manage/events'}>
-                        <img src={'/logo-text-only-white-text.png'} alt={''}/>
+                        <img src={'/Communitie.png'} alt={''}/>
                     </NavLink>
                 </div>
                 <div className={classes.breadcrumbs}>
@@ -175,7 +175,7 @@ const EventLayout = () => {
                 <div className={classes.logo}>
                     <NavLink to={'/manage/events'}>
                         <img style={{maxWidth: '170px', margin: "20px auto"}}
-                             src={'/logo.svg'} alt={''}/>
+                             src={'/Communitie.svg'} alt={''}/>
                     </NavLink>
                 </div>
                 <div className={classes.nav}>
